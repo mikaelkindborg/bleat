a99e4458689c6e0d8e3bd82714bf8a95990910a2
Rob Moran <rob.moran@arm.com>
2016-02-23 11:42:55 +0000

    Tidied branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglifyjs');
 
 gulp.task('lint', function() {
     return gulp.src(['dist/*.js', '!dist/*.min.js', '!dist/bleat.js'])
@@ -9,16 +7,4 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('release', function() {
-    return gulp.src(['dist/bleat.core.js', 'dist/bleat.*.js', '!dist/*.min.js'])
-        .pipe(concat('bleat.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(uglify('bleat.min.js', {
-            output: {
-                comments: /@license/
-            }
-        }))
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('default', ['lint', 'release']);
+gulp.task('default', ['lint']);
